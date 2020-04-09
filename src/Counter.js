4c18fd52930f5bca8eca23abcaf6d77a17a6dec1
Andrew Fritz <andrew.fritz86@gmail.com>
2020-04-09 09:46:58 -0400

    Add use effect hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [shmee, setShmee] = useState('andy');
+
+  useEffect(() => {
+    // Use effect is intended to handle any side effects of a render method
+    // If we think of functional components as pure, they should simply take
+    // props or state changes and render them in a a pure way. Anything else that might
+    // need to happen at the same time is a side effect, and should be handled here
+    console.log('running use effect');
+    // second arg is a list of dependencies. If anything in the list changes, run the hook
+    // empty list = run on mount
+    document.title = count;
+  }, [count]);
 
   const increment = () => {
     // We can pass a func to our updating functino, it takes the state
